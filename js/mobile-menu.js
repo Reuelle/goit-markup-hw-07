97f4a96135b0b3610e4e2169a2287cc98889daa4
Reuelle <ruel.paya@gmail.com>
2023-11-03 23:58:49 +0800

    menu.js

---
+++ js/mobile-menu.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.querySelector('.webpage');
+    const closeButton = document.getElementById('closeButton');
+
+    hamburger.addEventListener('click', function() {
+        nav.classList.toggle('mobile-menu');
+    });
+
+    closeButton.addEventListener('click', function() {
+        nav.classList.remove('mobile-menu');
+    });
+});
