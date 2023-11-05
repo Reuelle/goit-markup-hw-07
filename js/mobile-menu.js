2119bbbaf18065855fe82d1a6bf5450dd9f1d4d0
Reuelle <ruel.paya@gmail.com>
2023-11-05 20:33:26 +0800

    mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const closeButton = document.getElementById('closeButton');
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const mobileIcon = document.querySelector('.mobile-icon');
+    const mobileMenu = document.querySelector('.menu');
 
     // Toggle mobile menu when hamburger is clicked
     hamburger.addEventListener('click', function() {
         mobileMenu.classList.toggle('show-mobile-menu');
-        mobileIcon.style.display = 'none'; // Hide the hamburger icon
     });
 
     // Close mobile menu when close button is clicked
     closeButton.addEventListener('click', function() {
         mobileMenu.classList.remove('show-mobile-menu');
-        mobileIcon.style.display = 'block'; // Show the hamburger icon
     });
 });
+
