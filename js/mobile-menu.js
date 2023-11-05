44f6d0b7a6a660398006fafacae1e47371023a7e
Ruel <ruel.paya@gmail.com>
2023-11-06 07:47:06 +0800

    mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,17 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const closeButton = document.getElementById('closeButton');
     const mobileMenu = document.querySelector('.mobile-menu');
-    const mobileIcon = document.querySelector('.mobile-icon');
+    const contactInfo = document.querySelector('.contact-info');
+  const mobileIcon = document.querySelector('.mobile-icon');
 
     // Toggle mobile menu when hamburger is clicked
     hamburger.addEventListener('click', function() {
         mobileMenu.classList.toggle('show-mobile-menu');
-        mobileIcon.style.display = 'none'; // Hide the hamburger icon
+        hamburger.classList.add('hide-hamburger');
+        contactInfo.style.display = 'block'; // Hide the contact info
+      mobileIcon.style.display = "block";
     });
 
     // Close mobile menu when close button is clicked
     closeButton.addEventListener('click', function() {
         mobileMenu.classList.remove('show-mobile-menu');
-        mobileIcon.style.display = 'block'; // Show the hamburger icon
+        hamburger.classList.remove('hide-hamburger');
+        contactInfo.style.display = 'block'; // Show the contact info
     });
 });
+
