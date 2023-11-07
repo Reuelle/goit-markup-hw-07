858a032619fea759ec680fe40b0fcdc6e63d9c33
Ruel <ruel.paya@gmail.com>
2023-11-07 09:53:35 +0800

    mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,23 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
-    const closeButton = document.getElementById('closeButton');
     const mobileMenu = document.querySelector('.menu');
+    const mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
 
-    // Toggle mobile menu when hamburger is clicked
-    hamburger.addEventListener('click', function() {
+    hamburger.addEventListener('click', function(e) {
         mobileMenu.classList.toggle('show-mobile-menu');
+        mobileMenuOverlay.classList.toggle('show-overlay');
+        e.stopPropagation();
     });
 
-    // Close mobile menu when close button is clicked
-    closeButton.addEventListener('click', function() {
+    mobileMenuOverlay.addEventListener('click', function() {
         mobileMenu.classList.remove('show-mobile-menu');
+        mobileMenuOverlay.classList.remove('show-overlay');
+    });
+
+    document.addEventListener('click', function(e) {
+        if (!mobileMenu.contains(e.target) && !hamburger.contains(e.target)) {
+            mobileMenu.classList.remove('show-mobile-menu');
+            mobileMenuOverlay.classList.remove('show-overlay');
+        }
     });
 });
